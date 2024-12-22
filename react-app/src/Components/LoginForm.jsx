5406b5a5b3e7a5fd1afa2430ffee48191defadef
MK4 <mithunkarthik123@gmail.com>
2024-12-23 00:56:30 +0530

    Login_component

---
+++ react-app/src/Components/LoginForm.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./LoginForm.css"
+import { FaUser , FaLock , FaEye} from "react-icons/fa";
+
+
+const LoginForm = () => {
+  return (
+    <div className="wrapper">
+        <form action="">
+            <h1>
+                Login
+            </h1>
+            <div className="input-box">
+                <input type="text" placeholder='Username' />
+                <FaUser className='icon' />
+            </div>
+            <div className="input-box">
+                <input type="password" placeholder='Password' />
+                <FaEye  className='icon' />
+             </div>
+            <button>
+                Login
+            </button>
+        </form>
+    </div>
+  )
+}
+
+export default LoginForm
